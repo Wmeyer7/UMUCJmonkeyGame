9dfafc2783e368c02a3ad6045ad8e63e300b9851
Wmeyer7 <44577757+Wmeyer7@users.noreply.github.com>
2018-11-17 07:59:36 -0500

    Update Assets.java

--- HitTheBrack-core/src/htb/game/Assets.java
+++ HitTheBrack-core/src/htb/game/Assets.java
@@ -2,6 +2,7 @@ package htb.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -15,9 +16,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
  */
 public class Assets {
 	
-	public static Sound startScreenBGMusic; 		//To be looped on the start screen.
+	public static Music startScreenBGMusic; 		//To be looped on the start screen.
 	public static Sound buttonPressedSound;         //To be played when a button is pressed for Pause, Resume, Info... etc
-	public static Sound backgroundMusic;			//To be played while game is being played.
+	public static Music backgroundMusic;			//To be played while game is being played.
 	public static Sound bulletFired;				//To be played when a bullet is fired.
 	public static Sound bulletHitsWeakBrick;	    //To be played when a weak brick hit and player gains score!!
 	public static Sound bulletHitsStrongBrick;		//To be played when a strong brick is hit, and player loses score.
@@ -33,6 +34,8 @@ public class Assets {
 	//Declare the texture lists
 	public static Texture[] redSpider;
 	public static Texture[] normalSpider;
+	
+	
 	
 	
 	public static BitmapFont font;
@@ -53,6 +56,17 @@ public class Assets {
 		//Initialize the textures list
 		redSpider = new Texture[6];
 		normalSpider = new Texture[6];
+		
+		//Initialize Music
+		startScreenBGMusic = Gdx.audio.newMusic(Gdx.files.internal("sounds/music/MusicPlaceholder2.mp3"));
+		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("sounds/music/MusicPlaceholder3.mp3"));
+		
+		//Initialize SFX
+		buttonPressedSound = Gdx.audio.newSound(Gdx.files.internal("sounds/sfx/BlockDeathPH.wav"));
+		bulletFired = Gdx.audio.newSound(Gdx.files.internal("sounds/sfx/PlayerDeathPH.wav"));
+		bulletHitsWeakBrick = Gdx.audio.newSound(Gdx.files.internal("sounds/sfx/WrongBlockPH.wav"));
+		bulletHitsStrongBrick = Gdx.audio.newSound(Gdx.files.internal("sounds/sfx/PewPewPH.wav"));
+		
 		
 		for (int i=1; i<=6; i++){
 			redSpider[i-1] = new Texture(Gdx.files.internal("images/redSpider" + i+".png"));
