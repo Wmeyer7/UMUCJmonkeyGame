4054550879675fe103a15f28eb203331216f4228
Evan Smith <evan.matthew.smith@gmail.com>
2018-11-28 16:29:52 -0500

    Add files via upload
    Class for the win screen

---
+++ HitTheBrack-core/src/htb/stage/WinScreen.java
@@ -0,0 +1,46 @@
+package htb.stage;
+
+import htb.game.Assets;
+import htb.game.KillTheSpider;
+import htb.object.MyActor;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+
+public class WinScreen extends GameScreen {
+	
+	Label startLabel;
+	public static Texture infoScreen;
+	public static MyActor background;
+	
+	public WinScreen(KillTheSpider g) {
+		super(g);
+		create();
+	}
+
+	
+	public void create(){
+	    infoScreen = new Texture("images/placeholderWinScreen.PNG");
+		background = new MyActor();
+		background.setTexture(infoScreen);
+		background.setBounds(0, 0, mainStage.getWidth(), mainStage.getHeight());
+		mainStage.addActor(background);
+			
+	}
+	
+	public void update(float dt){
+		
+		if (Gdx.input.isKeyJustPressed(Keys.ANY_KEY)){
+			Assets.winMusic.stop();
+			game.getScreen().pause();
+			game.restart();
+		}
+	}
+	
+
+}
